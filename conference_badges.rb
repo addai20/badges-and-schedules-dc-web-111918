6e32148ec877bc670d9b4fafc3bf13b5afece913
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 02:10:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(attendees)
   return array
 end
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
